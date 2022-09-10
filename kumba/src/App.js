955a209e9cdaf209f12274973a9a8aeb299d7021
srikanth <kumbasrikanthi81@gmail.com>
2022-09-10 14:07:05 +0530

    secondcommit

--- kumba/src/App.js
+++ kumba/src/App.js
@@ -8,35 +8,28 @@ import Home from "./Components/Pages/Home/Home";
 import Login from "./Components/Log/Login";
 import Registration from "./Components/Registration/Registration";
 const App = () => {
- 
-
   return (
     <div>
-      
-        <Topbar />
-        
-          <Route path="/home">
-          
-            <Home />
-          </Route>
-          <Route path="/Registration">
-            <Registration />
-          </Route>
-          <Route path="/About">
-             
-            <About /> 
-          </Route>
-          <Route path="/Contact">
-            <Contact />
-          </Route>
-          <Route path="/Write">
-            <Write />
-          </Route>
-          <Route path="/Login">
-            <Login />
-          </Route>
-        
-      
+      <Topbar />
+
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/Registration">
+        <Registration />
+      </Route>
+      <Route path="/About">
+        <About />
+      </Route>
+      <Route path="/Contact">
+        <Contact />
+      </Route>
+      <Route path="/Write">
+        <Write />
+      </Route>
+      <Route path="/Login">
+        <Login />
+      </Route>
     </div>
   );
 };
